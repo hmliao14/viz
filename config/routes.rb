4cf2d39cd475394881bd8ff6f8b7c1d5f3e798a4
Carlynn <chick04sd@gmail.com>
2017-09-27 18:24:58 -0700

    setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/viz" => "viz#index", as: "viz"
+
+  resources :users do
+    resources :graph
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [{
+  first_name: "Chanel",
+  last_name: "Sparks",
+  current_city: "San Francisco",
+  image: "http://www.boostinspiration.com/wp-content/uploads/2010/09/BlackAndWhitePhotographyPeople1.jpg",
+  email: "csparks@unknown.com",
+  password_digest: "pizza",
+  }, {
+    first_name: "Janet",
+    last_name: "Sparks",
+    current_city: "San Francisco",
+    image: "https://i.pinimg.com/736x/8d/59/81/8d5981a2dbb27316afe9000066e8dbb2--yale-sands.jpg",
+    email: "jsparks@unknown.com",
+    password_digest: "pizza",
+    }]
+
+  User.create(users)

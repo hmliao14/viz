25bebdfbbd5d80760005d62427a1975e77846620
Carlynn <chick04sd@gmail.com>
2017-09-28 18:18:12 -0700

    show page for graphs showing all graphs data

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,11 +1,19 @@
 class GraphsController < ApplicationController
 
+  def index
+    @graphs = Graph.all
+  end
+
   def new
     @graph = current_user.graphs.build
   end
 
   def create
     @graph = current_user.graphs.build(graph_params)
+    @user = current_user
+    if @graph.save
+      redirect_to user_path(@user)
+    end
   end
 
   private

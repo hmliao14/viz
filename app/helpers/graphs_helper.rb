d56acdbf8b68984ec49e5662fd84d7531db67049
Carlynn <chick04sd@gmail.com>
2017-10-04 12:24:32 -0700

    descending order for graphs

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -1,24 +1,7 @@
 module GraphsHelper
 
   def render_bar_graph
-#     bar_chart @listing.group(@graph.x_axis).count, yAxis: {title: {text: 'hello'}}, height: "100%",
-#     ytitle: "Number of Job Listings",
-#     colors: ["#b00", "#666"],
-#      library: {
-#       download: 'column_chart',
-#       xtitle: @graph.x_axis.upcase,ytitle: "Number of Job Listings",
-#       yAxis: {
-#         title: {text: 'hellloooooooo'},
-#           ytitle: "Number of Job Listings",
-#           labels: {
-#               enabled: false
-#           }
-#       },
-#     }
-
-
     bar_chart @listing.group(:category).group(@graph.x_axis).count
-
   end
 
   def render_line_graph
